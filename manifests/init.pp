045fc1361a832cfd415d75fbbca6309584989210
Vitor Leal <vitorleal1@gmail.com>
2013-08-08 12:11:13 -0300

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,19 @@
+class geodjango {
+    Exec {
+      path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+    }
+
+    package {[
+        "libproj-dev",
+        "binutils",
+        "postgresql-9.1-postgis",
+        "gdal-bin",]:
+        ensure => installed,
+    }
+
+    file {"/tmp/install-postgis.sh":
+        ensure => present,
+        source => "puppet:///modules/geodjango/create_template_postgis.sh",
+        owner => postgres
+    }
+}
